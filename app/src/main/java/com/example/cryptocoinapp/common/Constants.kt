5daef1508ef24743cbf08005aa5addf80758f136
iuhasdan <iuhasdan@gmail.com>
2021-10-24 11:28:01 +0300

    Modified gradle options

--- app/src/main/java/com/example/cryptocoinapp/common/Constants.kt
+++ app/src/main/java/com/example/cryptocoinapp/common/Constants.kt
@@ -8,7 +8,4 @@ object Constants {
     const val PARAM_COIN_ID = "coinId"
 
     const val PARAM_COIN_PERIOD = "period"
-
-    const val API_KEY = "8f59007c-b6ef-4491-b866-7f5314a42587"
-
 }
--- app/src/main/java/com/example/cryptocoinapp/data/remote/CoinMarketService.kt
+++ app/src/main/java/com/example/cryptocoinapp/data/remote/CoinMarketService.kt
@@ -1,7 +1,7 @@
 package com.example.cryptocoinapp.data.remote
 
 
-import com.example.cryptocoinapp.common.Constants.API_KEY
+import com.example.cryptocoinapp.BuildConfig
 import com.example.cryptocoinapp.data.remote.dto.CoinDetailDto
 import com.example.cryptocoinapp.data.remote.dto.Data
 import retrofit2.Response
@@ -9,11 +9,11 @@ import retrofit2.http.*
 
 interface CoinMarketService {
 
-    @Headers("X-CMC_PRO_API_KEY: $API_KEY")
+    @Headers("X-CMC_PRO_API_KEY: ${BuildConfig.API_KEY}")
     @GET("v1/cryptocurrency/listings/latest")
     suspend fun getCoins(): Data
 
-    @Headers("X-CMC_PRO_API_KEY: $API_KEY")
+    @Headers("X-CMC_PRO_API_KEY: ${BuildConfig.API_KEY}")
     @GET("/v1/cryptocurrency/info?")
     suspend fun getCoinById(
         @Query("id") id: String
